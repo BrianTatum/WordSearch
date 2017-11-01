bd9059f11bb910163bd6b07ca657ec8c0dccca8f
Brian Tatum <brianttatum@gmail.com>
2017-10-31 22:41:22 -0500

    Components renders search grid from text.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './bootstrap/bootstrap.min.css';
 import './App.css';
 
+import GridLetter from './components/gridletter';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      grid: []
+    }
+  }
+
+  componentDidMount() {
+    this._loadGrid();
+  }
+
   render() {
+    const searchGrid = this._buildSearchGrid();
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="text-center">Hello World</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md justify-content-center d-flex">
+            <div className='grid-frame'>
+              {searchGrid}
+            </div>
+          </div>
+          <div className="col-md">
+          </div>
+        </div>
       </div>
     );
+  }
+
+  _buildSearchGrid() {
+    let rows = [];
+    this.state.grid.forEach((row, i) => {
+      rows.push(<div className='grid-row' key={i}>{this._buildGridRow(row)}</div>);
+    })
+    return rows;
+
+  }
+
+  _buildGridRow(gridRow) {
+    let row = [];
+    for(let i=0; i < gridRow.length; i++) {
+      row.push(<GridLetter letter={gridRow[i]} key={i} />);
+    }
+    return row;
+  }
+
+  _loadGrid() {
+    let text = `MMMMMMMMMM\nDDDDDDDDDD\nEEEEEEEEEE\nFFFFFFFFFF\nMMMMMMMMMM\nMMMMMMMMMM\nMMMMMMMMMM\nMMMMMMMMMM\nMMMMMMMMMM\nMMMMMMMMMM\n`;
+    let rows = text.match(/([a-zA-Z]+)/g);
+    let grid = [];
+    rows.forEach((row) => {
+      grid.push(row.split(''));
+    })
+    this.setState({grid: grid});
   }
 }
 
---
+++ src/components/gridletter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GridLetter = ({letter}) =>
+	<div className="grid-letter text-center d-inline-block">
+		{letter}
+	</div>
+
+export default GridLetter;
